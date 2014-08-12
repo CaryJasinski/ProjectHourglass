245bb9543edda14c8d9cf9701253b4c16d857711
Cary Jasinski <cary.jasinski@loop.colum.edu>
2014-08-12 13:24:26 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float acceleration = 0.5f;
+	public float maxVelocity = 8;
+	public float lookSpeed = 10;
+	public float jumpSpeed = 8;
+	public float upCastLength = 6;
+
+	private Rigidbody rigidPlayer;
+	private Vector3 playerDirection = Vector3.zero;
+	private Vector3 playerVelocity = Vector3.zero;
+	private Vector3 expectedPosition = Vector3.zero;
+	private Vector3 gravity = Vector3.zero;
+	private float distanceToGround = 0; 
+	private float clearence = 0; 
+
+	public float Clearence { get { return clearence; } }
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(new Ray(expectedPosition, -Vector3.up*distanceToGround));
+	}
+
+	void Start () 
+	{
+		rigidPlayer = transform.rigidbody;
+		distanceToGround = collider.bounds.extents.y;
+		gravity = Physics.gravity;
+	}
+
+	void FixedUpdate () 
+	{
+		DetectInput();
+		CalculateVelocity();
+		ApplyVelocity();
+		CalculateClearence();
+	}
+
+	void DetectInput()
+	{
+		playerDirection = DetermineDirection();
+
+		//TODO: Debug why jump isn't working
+		if(Input.GetKeyDown (KeyCode.Space))
+			playerVelocity.y = jumpSpeed;
+	}
+
+	Vector3 DetermineDirection()
+	{
+		Vector3 tempDirection = Vector3.zero;
+
+		if(Input.GetKey(KeyCode.W))
+			tempDirection += new Vector3(0, 0, 1);
+		if(Input.GetKey(KeyCode.A))
+			tempDirection += new Vector3(-1, 0, 0);
+		if(Input.GetKey(KeyCode.S))
+			tempDirection += new Vector3(0, 0, -1);
+		if(Input.GetKey(KeyCode.D))
+			tempDirection += new Vector3(1, 0, 0);
+
+		if(tempDirection != Vector3.zero)
+		{
+			tempDirection.Normalize();
+			ChangeLookDirection(tempDirection);
+		}
+
+		return tempDirection;
+	}
+
+	void ChangeLookDirection (Vector3 lookDirection)
+	{
+		transform.rotation = Quaternion.Lerp (transform.rotation,  Quaternion.LookRotation(lookDirection), Time.fixedDeltaTime * lookSpeed);
+	}
+	
+	void CalculateVelocity ()
+	{
+		playerVelocity = rigidPlayer.velocity;
+		if(playerVelocity.magnitude < maxVelocity)
+			playerVelocity += playerDirection * acceleration;
+		//else
+			//playerVelocity = playerDirection * maxVelocity;
+
+		playerVelocity += gravity * Time.fixedDeltaTime;
+	}
+
+	void ApplyVelocity ()
+	{
+		if(CanMoveTo())
+			rigidPlayer.velocity = playerVelocity;
+		else
+			rigidPlayer.velocity = Vector3.zero;
+	}
+
+	bool CanMoveTo ()
+	{
+		expectedPosition = playerDirection/2 + transform.position;
+		return Physics.Raycast(expectedPosition, -Vector3.up, distanceToGround + 2);
+	}
+
+	void CalculateClearence ()
+	{
+		RaycastHit hit;
+		Ray upRay = new Ray(transform.position, Vector3.up * upCastLength);
+		if (Physics.Raycast(upRay, out hit))
+			clearence = hit.distance;
+		else
+			clearence = upCastLength;
+	}
+}
---
+++ Assets/Scripts/SmoothFollowTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowTarget : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 followOffset;
+	public float followSpeed = 1;
+
+	private Vector3 targetPosition;
+	private Vector3 velocity;
+	private PlayerController playerController;
+
+	void Start ()
+	{
+		if(target.CompareTag("Player"))
+			playerController = target.GetComponent<PlayerController>();
+	}
+
+	void FixedUpdate () 
+	{
+		targetPosition = target.position + followOffset;
+		if(target.CompareTag("Player"))
+			targetPosition.y = target.position.y + playerController.Clearence - 1;
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
+	}
+}
